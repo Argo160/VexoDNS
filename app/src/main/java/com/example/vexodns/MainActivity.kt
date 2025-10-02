6bb700f1c7e2310699652bb95c720666cd240c37
argo160 <shah0rum@gmail.com>
2025-10-03 01:24:58 +0330

    Debugging Localized languages and few Graphics issue

--- app/src/main/java/com/example/vexodns/MainActivity.kt
+++ app/src/main/java/com/example/vexodns/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.vexodns
 import android.os.Bundle
 import android.view.Menu
 import android.content.Context
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vexodns.databinding.ActivityMainBinding
